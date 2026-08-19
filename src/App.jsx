00b2aeb0cbc362c0d7c0e647e7bdf6dcc7930340
holheb7286 <holheb7286@students.ecpi.edu>
2026-08-19 12:53:02 -0400

    Initial Cozy Garden React app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Garden!</h1>
+      <p>A tiny React garden, growing one component at a time.</p>
+    </div>
+  );
+}
+
+export default App;
